f077538dedb691bd411bc9179c0bef8ee28ae860
Ahmed Magdy <ahmedmagdee04@gmail.com>
2025-04-03 09:07:43 +0200

    using React Highlighter Library for create a FileSyntaxHighlighter.tsx for enhance the display of content and make it dynamic

--- src/components/FileSyntaxHighlighter.tsx
+++ src/components/FileSyntaxHighlighter.tsx
@@ -1,26 +1,27 @@
-// import SyntaxHighlighter from "react-syntax-highlighter/dist/esm/default-highlight";
-// import { atomOneDark } from "react-syntax-highlighter/dist/esm/styles/hljs";
-// interface IProps {
-//   content: string | undefined;
-// }
+import SyntaxHighlighter from "react-syntax-highlighter/dist/esm/default-highlight";
+import { atomOneDark } from "react-syntax-highlighter/dist/esm/styles/hljs";
 
-// const FileSyntaxHighlighter = ({ content }: IProps) => {
-//   return (
-//     <SyntaxHighlighter
-//       language="javascript"
-//       style={atomOneDark}
-//       customStyle={{
-//         backgroundColor: "transparent",
-//         width: "100%",
-//         maxHeight: "100vh",
-//         overflowX: "auto",
-//         fontSize: "1.5rem",
-//       }}
-//       showLineNumbers
-//     >
-//       {String(content)}
-//     </SyntaxHighlighter>
-//   );
-// };
+interface IProps {
+  content: string | undefined;
+}
 
-// export default FileSyntaxHighlighter;
+const FileSyntaxHighlighter = ({ content }: IProps) => {
+  return (
+    <SyntaxHighlighter
+      language="javascript"
+      style={atomOneDark}
+      customStyle={{ backgroundColor: "transparent",
+        width: "100%",
+        maxHeight: "100vh",
+        overflowX: "auto",
+        fontSize: "0.9rem",
+       }}
+       showLineNumbers
+    >
+      {content || ""}
+      {/* {String(content)} */}
+    </SyntaxHighlighter>
+  );
+};
+
+export default FileSyntaxHighlighter;
